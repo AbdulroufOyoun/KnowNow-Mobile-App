d03d8503d9bd392d73a904fb6e22ac40af4d0878
AbdOyoun <oyoun26@gmail.com>
2026-01-22 20:03:02 +0300

    remove image pemision

--- router/data.tsx
+++ router/data.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const url = 'https://learnnowsyria.com/api/';
-// const url = 'http://192.168.1.5:8888/api/';
+// const url = 'http://192.168.1.8:8888/api/';
 
 export async function getUserData(): Promise<string | null> {
   try {
